f32d4d667df7de0211e1656c0314b586074b6d26
ulysses-ck <ulisesapaza.280@gmail.com>
2024-05-25 22:29:32 -0300

    fix: add a legend

--- src/components/ScenePlayer/index.tsx
+++ src/components/ScenePlayer/index.tsx
@@ -6,61 +6,64 @@ interface ScenePlayerProps {
 
 export default function ScenePlayer(props: ScenePlayerProps) {
   return (
-    <div className="scenePlayer">
-      <div
-        className="stickHorizontal"
-        style={{
-          backgroundColor: props.tries <= 8 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="stickVertical"
-        style={{
-          backgroundColor: props.tries <= 7 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="rope"
-        style={{
-          backgroundColor: props.tries <= 6 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="head"
-        style={{
-          backgroundColor: props.tries <= 5 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="body"
-        style={{
-          backgroundColor: props.tries <= 4 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="leftArm"
-        style={{
-          backgroundColor: props.tries <= 3 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="rightArm"
-        style={{
-          backgroundColor: props.tries <= 2 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="leftLeg"
-        style={{
-          backgroundColor: props.tries <= 1 ? "#000000" : "transparent",
-        }}
-      ></div>
-      <div
-        className="rightLeg"
-        style={{
-          backgroundColor: props.tries <= 0 ? "#000000" : "transparent",
-        }}
-      ></div>
-    </div>
+    <>
+      <h2>{props.tries > 0 ? "You are still surviving" : "You are die"}</h2>
+      <div className="scenePlayer">
+        <div
+          className="stickHorizontal"
+          style={{
+            backgroundColor: props.tries <= 8 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="stickVertical"
+          style={{
+            backgroundColor: props.tries <= 7 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="rope"
+          style={{
+            backgroundColor: props.tries <= 6 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="head"
+          style={{
+            backgroundColor: props.tries <= 5 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="body"
+          style={{
+            backgroundColor: props.tries <= 4 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="leftArm"
+          style={{
+            backgroundColor: props.tries <= 3 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="rightArm"
+          style={{
+            backgroundColor: props.tries <= 2 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="leftLeg"
+          style={{
+            backgroundColor: props.tries <= 1 ? "#000000" : "transparent",
+          }}
+        ></div>
+        <div
+          className="rightLeg"
+          style={{
+            backgroundColor: props.tries <= 0 ? "#000000" : "transparent",
+          }}
+        ></div>
+      </div>
+    </>
   );
 }
