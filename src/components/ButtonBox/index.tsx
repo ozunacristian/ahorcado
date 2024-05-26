8604fa23014c779e34ce22627855b60ef6c38b1f
ulysses-ck <ulisesapaza.280@gmail.com>
2024-05-26 13:20:14 -0300

    fix: replace with keyboardbutton old logic

--- src/components/ButtonBox/index.tsx
+++ src/components/ButtonBox/index.tsx
@@ -1,27 +1,26 @@
+import KeyboardButton from "../KeyboardButton";
 import "./ButtonBox.css";
 
 interface ButtonBoxProps {
   setPlayerInput: (input: string) => void;
+  tries: number;
 }
 
 export default function ButtonBox(props: Readonly<ButtonBoxProps>) {
-  const handleOnClick = (char: string) => {
-    props.setPlayerInput(char);
-  };
+  const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(65 + i));
 
-  const generateButtons = () => {
-    // generateButtons is a function that returns an array of buttons with the letters of the alphabet
-    const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(65 + i));
-
-    return alphabet.map((letter) => (
-      <button
-        key={letter}
-        onClick={() => handleOnClick(letter)}
-        className="button"
-      >
-        {letter}
-      </button>
-    ));
-  };
-  return <section className="buttonBox">{generateButtons()}</section>;
+  return (
+    <section className="buttonBox">
+      {alphabet.map((letter) => {
+        return (
+          <KeyboardButton
+            key={letter}
+            tries={props.tries}
+            letter={letter}
+            setPlayerInput={props.setPlayerInput}
+          />
+        );
+      })}
+    </section>
+  );
 }
