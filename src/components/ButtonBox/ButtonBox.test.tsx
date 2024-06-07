6527c5efdcda05d32c05701a14cf86ce697cd8bd
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-07 19:53:23 -0300

    feat: create test for buttonbox

--- src/components/ButtonBox/ButtonBox.test.tsx
+++ src/components/ButtonBox/ButtonBox.test.tsx
@@ -1,15 +1,25 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import ButtonBox from ".";
 import { render, screen } from "@testing-library/react";
 
 describe("ButtonBox", () => {
-    it("ButtonBox should be disabled once being selected", () => {
-      render(<ButtonBox setPlayerInput={(input)=>{
-        console.log(input)
-      }} tries={1}/>);
-  
-      expect(Array.from(screen.getByTestId("head").classList)).toContain("head");
-    });
-  
-    
+  it("ButtonBox should be disabled once being selected", () => {
+    render(
+      <ButtonBox
+        setPlayerInput={(input) => {
+          console.log(input);
+        }}
+        tries={1}
+      />
+    );
+
+    const buttonJ = screen.getByTestId("keyboardButton-J");
+
+    buttonJ.click();
+
+    screen.debug();
+
+    // should have disabled after click
+    expect(buttonJ.getAttribute("disabled")).toBe("");
   });
+});
